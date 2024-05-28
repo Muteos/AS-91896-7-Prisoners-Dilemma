213b2f2f70445781792059109350546897ae3898
Russell Zhang <zhangru@whs.school.nz>
2024-05-28 14:39:00 +1200

    Frist time

---
+++ Players.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Players here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class Players
+{
+ Scanner sc = new Scanner(System.in);
+ String p1Name = sc.nextLine();
+ 
+ 
+ 
+ 
+}
