ecca09cf52d74aff6150be176a473f186df3cad8
Muteos <zhangru@whs.school.nz>
2024-06-10 09:34:53 +1200

    Update Players.java (6.10)
    I add some comment to my previous code and write up the how robot strategy work

--- Prisoners dilemma v.5.16/Players.java
+++ Prisoners dilemma v.5.16/Players.java
@@ -9,17 +9,23 @@ import java.util.Scanner;
  public class Players{
     public static void main(String[] args){
         //Initialize player
-        System.out.println("May I have your name?");
-        Scanner sc = new Scanner(System.in);
-        String p1Name = sc.nextLine();
-        System.out.println("Hello, " + p1Name+ " you are playing Prisoners dilemma now");
+        System.out.println("May I have your name?");	//Collect user's name
+        Scanner sc = new Scanner(System.in); 	//Create scanner
+        String p1Name = sc.nextLine();	//Read user's name with scanner
+        System.out.println("Hello, " + p1Name+ " you are playing Prisoners dilemma now");	//Print out the greeting sentence
         //Game loop
-        for (int round = 1;round<=5; round++) {
-            System.out.println("Round" + round);
-            System.out.println("Enter your choice (C for cooperate, D for defect)");
-            String humanChoice =  sc.nextLine();
-            char humanStrategy = humanChoice.charAt(0);
-            char 
+        for (int round = 1;round<=5; round++) {	
+            System.out.println("Round" + round);	//Round number output
+            System.out.println("Enter your choice (C for cooperate, D for defect)");	//Ask users for their choice
+            String humanChoice =  sc.nextLine();	//Read user's choice
+            char humanStrategy = humanChoice.charAt(0);	//Preventing incorrect input
+            char robotStrategy
+				if (round == 1)	{
+				robotChoice = 'C'
+					}
+			
+				
+			
         }
         
         
